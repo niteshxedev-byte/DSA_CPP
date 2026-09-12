f17e2078938c2d3b66fd2c66e170ca59f2bccdab
Nitesh Kumar Vashisht <niteshxe.dev@gmail.com>
2026-09-13 01:24:03 +0530

    pattern hard 01 to 05 questions

---
+++ PATTERN_PROGRAMMING/dsa_hard_pattern_programmiing.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+void pattern1(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      cout << " " << " ";
+    }
+
+    for (int j = 0; j <= i; j++) {
+      cout << "*" << " ";
+    }
+    cout << endl;
+  }
+}
+
+void pattern2(int n) {
+  for (int i = 1; i <= n - 1; i++) {
+    for (int j = 0; j < n - i; j++) {
+      cout << " " << " ";
+    }
+    for (int j = 1; j <= i; j++) {
+      cout << i << " ";
+    }
+    cout << endl;
+  }
+}
+
+void pattern3(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      cout << " " << " ";
+    }
+    for (int j = 1; j <= i; j++) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+
+void patter4(int n) {
+  for (int i = 1; i <=n; i++) {
+
+    for (int j = 0; j < n - i; j++) {
+      cout << " " << " ";
+    }
+    for (int j = 0; j < i; j++) {
+      char ch = 'A' + j;
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+
+void patter5(int n) {
+  for (int i =1 ; i <= n; i++) {
+    for (int j = 0; j < n - i ; j++) {
+      cout << " " << " ";
+    }
+    for (int j = i; j >=1; j--) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+
+
+
+
+int main() {
+  int n = 5;
+
+    pattern1(n);
+    pattern2(n);
+  pattern3(n);
+  patter4(n);
+  patter5(n);
+   patter6(n); // 13-09-2026
+
+  return 0;
+}
