3f8ef36f9962dc0018a0ddb994972b962f65f782
Nitesh Kumar Vashisht <niteshxe.dev@gmail.com>
2026-09-15 22:46:50 +0530

    hard patten

--- PATTERN_PROGRAMMING/dsa_hard_pattern_programmiing.cpp
+++ PATTERN_PROGRAMMING/dsa_hard_pattern_programmiing.cpp
@@ -64,18 +64,65 @@ void patter5(int n) {
   }
 }
 
+void patter6( int n){
+  for(int i = 0 ; i<n;i++){
+    for(int j = 1;j<n-i;j++){
+      cout<<"_"<<" ";
+    }
+     for(int j = 0;j<=i;j++){
+      char ch  = 'A'+i ;
+      cout<<ch<<" ";
+    }
+    cout<<endl;
+  }
+} 
 
+
+void patter7(int n ){
+  for(int  i = 0 ; i <=  n ; i++){
+
+    for(int j = 0 ;j<n-i;j++){
+      cout<<" "<<" ";
+    }
+
+char ch = 'A'+n-1;
+    for(int j= 0 ; j<i;j++){
+      
+
+      cout<<ch<<" ";
+      ch--;
+    }
+    cout<<endl;
+  }
+}
+
+
+void patter8(int n ){
+  for(int  i = 0 ; i <=  n ; i++){
+
+    for(int j = 0 ;j<n-i;j++){
+      cout<<" "<<" ";
+    }
+
+    for(int j= n ; j>n-i;j--){
+      cout<<j<<" ";
+    }
+    cout<<endl;
+  }
+}
 
 
 int main() {
   int n = 5;
 
-    pattern1(n);
-    pattern2(n);
-  pattern3(n);
-  patter4(n);
-  patter5(n);
-   patter6(n); // 13-09-2026
+  //   pattern1(n);
+  //   pattern2(n);
+  // pattern3(n);
+  // patter4(n);
+  // patter5(n);
+  //  patter6(n); 
+// patter7(n );
 
+// patter8(n );
   return 0;
 }
